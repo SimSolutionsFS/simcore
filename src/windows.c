5fe452ca6743182df4095dfa4306c4d4e7de2a11
slimit75 <hi@slimit75.me>
2026-09-10 17:13:25 -0400

    fix(windows): fixed changed window parameter name

--- src/windows.c
+++ src/windows.c
@@ -79,7 +79,7 @@ void xpd_win_create2(xpd_win_t *inWindow, const char *title, int winLeft, int wi
 	inWindow->params.top = inWindow->params.bottom + inWindow->height - 20;
 
 #ifdef XPLM440
-	inWindow->params.windowContentType = xplm_WindowContentTypePanelGraphics;
+	inWindow->params.contentType = xplm_WindowContentTypePanelGraphics;
 #endif
 
 	inWindow->windowID = XPLMCreateWindowEx(&inWindow->params);
